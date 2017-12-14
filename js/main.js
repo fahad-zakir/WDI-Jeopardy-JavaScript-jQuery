f4d9bc4d3dc918a650571996f7568625ef37f1dd
Fahad Zakir <fahadzakir81@gmail.com>
2017-12-14 13:23:20 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,132 +4,167 @@
     });
 
 
-    function modalFunctionForQuestionsOne() {
+    var count = 0;
+    var countEl = document.getElementById("count");
+
+    function modalFunctionForQuestionOne() {
         var txt;
         var person = prompt('What does HTML stand for');
         if (person == null || person == "") {
 
-        } else if (person === 'Hypertext Markup Language') {
+        } else if (person.toLowerCase() === 'hypertext markup language') {
             txt = 'Congragulations! You get $200';
+            count += 200;
+            countEl.innerHTML = count;
+            setTimeout(function(){
+              $(".close-modal").click();
+            }, 500);
         } else {
             txt = 'Good Try! but that was incorrect.';
         }
         document.getElementById("html-200").innerHTML = txt;
     }
 
-    function modalFunctionForQuestionsTwo() {
+    function modalFunctionForQuestionTwo() {
         var txt;
         var person = prompt('What are the HTML elements represented by?');
         if (person == null || person == "") {
 
-        } else if (person === 'Tags') {
+        } else if (person.toLowerCase() === 'tags') {
             txt = 'Congragulations! You get $500';
+            count += 500;
+            countEl.innerHTML = count;
+            setTimeout(function(){
+              $(".close-modal").click();
+            }, 500);
         } else {
             txt = 'Good Try! but that was incorrect.';
         }
         document.getElementById("html-500").innerHTML = txt;
     }
 
-    function modalFunctionForQuestionsThree() {
+    function modalFunctionForQuestionThree() {
         var txt;
         var person = prompt('What is the root element of an HTML page');
         if (person == null || person == "") {
 
-        } else if (person === '<html>') {
+        } else if (person.toLowerCase() === '<html>') {
             txt = 'Congragulations! You get $1000';
+            count += 1000;
+            countEl.innerHTML = count;
+            setTimeout(function(){
+              $(".close-modal").click();
+            }, 500);
         } else {
             txt = 'Good Try! but that was incorrect.';
         }
         document.getElementById("html-1000").innerHTML = txt;
     }
 
-    function modalFunctionForQuestionsFour() {
+    function modalFunctionForQuestionFour() {
         var txt;
         var person = prompt('What does CSS stand for');
         if (person == null || person == "") {
 
-        } else if (person === 'Cascading Style Sheets') {
+        } else if (person.toLowerCase() === 'cascading style sheets') {
             txt = 'Congragulations! You get $200';
+            count += 200;
+            countEl.innerHTML = count;
+            setTimeout(function(){
+              $(".close-modal").click();
+            }, 500);
         } else {
             txt = 'Good Try! but that was incorrect.';
         }
         document.getElementById("css-200").innerHTML = txt;
     }
 
-    function modalFunctionForQuestionsFive() {
+    function modalFunctionForQuestionFive() {
         var txt;
         var person = prompt('Who oficially recommended CSS1 in 1995?');
         if (person == null || person == "") {
 
-        } else if (person === 'W3C') {
+        } else if (person.toLowerCase() === 'w3c') {
             txt = 'Congragulations! You get $500';
+            count += 500;
+            countEl.innerHTML = count;
+            setTimeout(function(){
+              $(".close-modal").click();
+            }, 500);
         } else {
             txt = 'Good Try! but that was incorrect.';
         }
         document.getElementById("css-500").innerHTML = txt;
     }
 
-    function modalFunctionForQuestionsSix() {
+    function modalFunctionForQuestionSix() {
         var txt;
         var person = prompt('How do you call a class with the name slam in a CSS style.sheet?');
         if (person == null || person == "") {
 
-        } else if (person === '.slam') {
+        } else if (person.toLowerCase() === '.slam') {
             txt = 'Congragulations! You get $1000';
+            count += 1000;
+            countEl.innerHTML = count;
+            setTimeout(function(){
+              $(".close-modal").click();
+            }, 500);
         } else {
             txt = 'Good Try! but that was incorrect.';
         }
         document.getElementById("css-1000").innerHTML = txt;
     }
 
-    function modalFunctionForQuestionsSeven() {
+    function modalFunctionForQuestionSeven() {
         var txt;
         var person = prompt('What is declared to store data in JavaScript');
         if (person == null || person == "") {
 
-        } else if (person === 'Variable') {
+        } else if (person.toLowerCase() === 'variable') {
             txt = 'Congragulations! You get $200';
+            count += 200;
+            countEl.innerHTML = count;
+            setTimeout(function(){
+              $(".close-modal").click();
+            }, 500);
         } else {
             txt = 'Good Try! but that was incorrect.';
         }
         document.getElementById("js-200").innerHTML = txt;
     }
 
-    function modalFunctionForQuestionsEight() {
+    function modalFunctionForQuestionEight() {
         var txt;
-        var person = prompt('Who is the founder of JavaScript?');
+        var person = prompt('Who is the founder of JavaScript');
         if (person == null || person == "") {
 
-        } else if (person === 'Brendan Eich') {
+        } else if (person.toLowerCase() === 'brendan eich') {
             txt = 'Congragulations! You get $500';
+            count += 500;
+            countEl.innerHTML = count;
+            setTimeout(function(){
+              $(".close-modal").click();
+            }, 500);
         } else {
             txt = 'Good Try! but that was incorrect.';
         }
         document.getElementById("js-500").innerHTML = txt;
     }
 
-    function modalFunctionForQuestionsNine() {
+    function modalFunctionForQuestionNine() {
         var txt;
         var person = prompt('What comes up when you add 0.1 and 0.2 in the console?');
         if (person == null || person == "") {
 
         } else if (person === '0.30000000000000004') {
             txt = 'Congragulations! You get $1000';
+            count += 1000;
+            countEl.innerHTML = count;
+            setTimeout(function(){
+              $(".close-modal").click();
+            }, 500);
         } else {
             txt = 'Good Try! but that was incorrect.';
         }
         document.getElementById("js-1000").innerHTML = txt;
     }
-
- 
- var count = 0;
- var countEl = document.getElementById("count");
-
- function plus() {
-     console.log("plus is clicked!")
-     count += 200;
-     countEl.value = count;
- }
-
- 
-     
