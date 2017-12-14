9ed1dd9bbb62224f4d972dea7ed441b2378b594f
Fahad Zakir <fahadzakir81@gmail.com>
2017-12-13 23:16:11 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 $(document).ready(function () {
     console.log("ready!");
 });
+
+let score = 0;
+score += 200
+console.log(score);
 function myFunction() {
     var txt;
     var person = prompt('What does HTML stand for');
@@ -13,4 +17,18 @@ function myFunction() {
         txt = 'Good Try! but that was incorrect.';
     }
     document.getElementById("new").innerHTML = txt;
+}
+var count = 200;
+var countEl = document.getElementById("count");
+
+function plus() {
+    count += 200;
+    countEl.value = count;
+}
+
+function minus() {
+    if (count > 1) {
+        count--;
+        countEl.value = count;
+    }
 }
