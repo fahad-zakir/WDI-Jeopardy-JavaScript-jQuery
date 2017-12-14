97ec72cd1121f848a0e73573f83bdfa72bea940d
fahad-zakir <fahadzakir81@gmail.com>
2017-12-14 10:03:32 -0500

    project is in the final stage with all the updates

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-build-a-portfolio
-# front-end-portfolio
+# WDI- Jeopardy
+## Project Description
+This is online-Jeopardy is based off the classic game of Jeopardy.  The short quiz has questions related to  Web-Development's three popular languages.  Cick on $__box to start the quiz. If you enter the correct answer a message will appear for the amount you earned. If incorrect click button again to try again, or click
+
+# Trello Board Link
+https://trello.com/b/73odC5z5/wdi-jeopardy
+
+
+
+
+                   
--- js/main.js
+++ js/main.js
@@ -1,34 +1,137 @@
-// A $( document ).ready() block.
-$(document).ready(function () {
-    console.log("ready!");
-});
+    // A $( document ).ready() block.
+    $(document).ready(function () {
+        console.log("ready!");
+    });
 
-let score = 0;
-score += 200
-console.log(score);
-function myFunction() {
-    var txt;
-    var person = prompt('What does HTML stand for');
-    if (person == null || person == "") {
-    
-    } else if (person === 'Hypertext Markup Language') {
-        txt = 'Congragulations! You get $200';   
-    } else {
-        txt = 'Good Try! but that was incorrect.';
+
+    function myFunction() {
+        var txt;
+        var person = prompt('What does HTML stand for');
+        if (person == null || person == "") {
+
+        } else if (person === 'Hypertext Markup Language') {
+            txt = 'Congragulations! You get $200';
+        } else {
+            txt = 'Good Try! but that was incorrect.';
+        }
+        document.getElementById("html-200").innerHTML = txt;
     }
-    document.getElementById("new").innerHTML = txt;
-}
-var count = 200;
-var countEl = document.getElementById("count");
 
-function plus() {
-    count += 200;
-    countEl.value = count;
-}
+    function myFunctionTwo() {
+        var txt;
+        var person = prompt('What are the HTML elements represented by?');
+        if (person == null || person == "") {
 
-function minus() {
-    if (count > 1) {
-        count--;
+        } else if (person === 'Tags') {
+            txt = 'Congragulations! You get $500';
+        } else {
+            txt = 'Good Try! but that was incorrect.';
+        }
+        document.getElementById("html-500").innerHTML = txt;
+    }
+
+    function myFunctionThree() {
+        var txt;
+        var person = prompt('What is the root element of an HTML page');
+        if (person == null || person == "") {
+
+        } else if (person === '<html>') {
+            txt = 'Congragulations! You get $1000';
+        } else {
+            txt = 'Good Try! but that was incorrect.';
+        }
+        document.getElementById("html-1000").innerHTML = txt;
+    }
+
+    function myFunctionFour() {
+        var txt;
+        var person = prompt('What does CSS stand for');
+        if (person == null || person == "") {
+
+        } else if (person === 'Cascading Style Sheets') {
+            txt = 'Congragulations! You get $200';
+        } else {
+            txt = 'Good Try! but that was incorrect.';
+        }
+        document.getElementById("css-200").innerHTML = txt;
+    }
+
+    function myFunctionFive() {
+        var txt;
+        var person = prompt('Who oficially recommended CSS1 in 1995?');
+        if (person == null || person == "") {
+
+        } else if (person === 'W3C') {
+            txt = 'Congragulations! You get $500';
+        } else {
+            txt = 'Good Try! but that was incorrect.';
+        }
+        document.getElementById("css-500").innerHTML = txt;
+    }
+
+    function myFunctionSix() {
+        var txt;
+        var person = prompt('How do you call a class with the name slam in a CSS style.sheet?');
+        if (person == null || person == "") {
+
+        } else if (person === '.slam') {
+            txt = 'Congragulations! You get $1000';
+        } else {
+            txt = 'Good Try! but that was incorrect.';
+        }
+        document.getElementById("css-1000").innerHTML = txt;
+    }
+
+    function myFunctionSeven() {
+        var txt;
+        var person = prompt('What is declared to store data in JavaScript');
+        if (person == null || person == "") {
+
+        } else if (person === 'Variable') {
+            txt = 'Congragulations! You get $200';
+        } else {
+            txt = 'Good Try! but that was incorrect.';
+        }
+        document.getElementById("js-200").innerHTML = txt;
+    }
+
+    function myFunctionEight() {
+        var txt;
+        var person = prompt('Who is the founder of JavaScript');
+        if (person == null || person == "") {
+
+        } else if (person === 'Brendan Eich') {
+            txt = 'Congragulations! You get $500';
+        } else {
+            txt = 'Good Try! but that was incorrect.';
+        }
+        document.getElementById("js-500").innerHTML = txt;
+    }
+
+    function myFunctionNine() {
+        var txt;
+        var person = prompt('What comes up when you add 0.1 and 0.2 in the console?');
+        if (person == null || person == "") {
+
+        } else if (person === '0.30000000000000004') {
+            txt = 'Congragulations! You get $1000';
+        } else {
+            txt = 'Good Try! but that was incorrect.';
+        }
+        document.getElementById("js-1000").innerHTML = txt;
+    }
+
+    var count = 200;
+    var countEl = document.getElementById("count");
+
+    function plus() {
+        count += 200;
         countEl.value = count;
     }
-}
+
+    function minus() {
+        if (count > 200) {
+            count -= 200;
+            countEl.value = count;
+        }
+    }
